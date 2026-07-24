e913f2e0aa00c9f68f87b6ef806be3d0c635164b
cedenin22-lab <cedenin22@gmail.com>
2026-07-24 12:30:42 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'html2pdf.js': '/node_modules/html2pdf.js/dist/html2pdf.bundle.min.js',
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['html2pdf.js'],
   },
 });
